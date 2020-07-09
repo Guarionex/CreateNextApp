b792c29e08e6507446a7fba97c598ebdfe655eb0
Edwin <chespirana@gmail.com>
2020-07-09 17:43:24 -0500

    Basic Test Render test

--- nextjs-blog/test/pages/index.spec.js
+++ nextjs-blog/test/pages/index.spec.js
@@ -1,6 +1,24 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+
+import Home from "../../pages";
 
 describe('Index Page', () => {
+    let testRenderer ,
+        testInstance ;
+
+    beforeEach(() => {
+        testRenderer  = TestRenderer.create(<Home />);
+        testInstance = testRenderer.root;
+    });
+
    it('Should pass test', () => {
        expect(true).toBeTruthy();
+   });
+
+   it('Should have h1', () => {
+      let renderedh1 = testInstance.findByType('h1');
+
+      expect(renderedh1.children[0]).toBe('Welcome to ');
    });
 });
